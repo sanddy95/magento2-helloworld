9bf595cc0cbbd0f755d7e11c20509c473df22dca
sandy <sandeepshah1995@gmail.com>
2019-03-07 16:11:58 +0530

    hellworld first commit

---
+++ Controller/Index/Index.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Sandeep\HelloWorld\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Shipping\Model\Rate\ResultFactory;
+
+class Index extends Action{
+
+    protected $resultPageFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ){
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        $this->_view->loadLayout();
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
+    }
+}
